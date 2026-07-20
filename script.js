37f1fad454efe030f9e5e3d954da0ba7f99a1228
Deepak <ds6248994@gmail.com>
2026-07-20 09:58:41 +0530

    Added Dark Mode

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ document.getElementById("btn").addEventListener("click", function () {
 
 window.open("https://wa.me/918750476209","_blank");
 
+}
+
+const themeBtn = document.getElementById("theme-btn");
+
+themeBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        themeBtn.innerHTML="☀️ Light Mode";
+    }else{
+        themeBtn.innerHTML="🌙 Dark Mode";
+    }
+
 });
